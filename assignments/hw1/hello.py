642c9d65a556d240e657dfd796df8595d8f1751c
Reginald Wigfall <wigfallrd@g.cofc.edu>
2021-09-06 12:32:01 -0400

    completed hw1

---
+++ assignments/hw1/hello.py
@@ -0,0 +1,12 @@
+""" """ """
+Name: Reginald Wigfall
+hello.py
+
+Problem: Print "hello, world!" to the screen
+Certification of Authenticity:
+I certify that this assignment  is entirely my own work
+""" """ """
+
+def main():
+    print("hello, world!")
+main()
